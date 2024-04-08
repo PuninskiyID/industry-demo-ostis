e0ee9f7af7d1424fadc192c53be671a7fae88ce8
PuninskiyID <ipuninskiy@gmail.com>
2024-04-08 23:41:43 +0300

    ims.ostis.kb dir fix

--- scripts/install_scp.sh
+++ scripts/install_scp.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 # Define the path to the destination directory
 destination_dir="../ostis-web-platform/scp-machine"
-destination_dir="../ostis-web-platform/ims.ostis.kb"
+destination_dir2="../ostis-web-platform/ims.ostis.kb"
 
 # Clone the SCP-Machine repository into the destination directory
 git clone https://github.com/ostis-ai/scp-machine.git "$destination_dir"
