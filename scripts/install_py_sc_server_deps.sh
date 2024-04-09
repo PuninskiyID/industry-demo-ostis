a5e5ee8cb4628a82d7c55486018a1c11d0e6cd24
Kl1rik <kirillshershan@gmail.com>
2024-04-09 15:46:04 -0400

    Add sudo

--- scripts/install_py_sc_server_deps.sh
+++ scripts/install_py_sc_server_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
-apt -y  install portaudio19-dev
+sudo apt -y  install portaudio19-dev
 pip3 install -r "$APP_ROOT_PATH/problem-solver/py/requirements.txt"
